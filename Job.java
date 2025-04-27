53db4e0b9f6613a93dd202525e217ebc3aab56e2
Dinesh-oo6 <dinexhoo6oo@gmail.com>
2025-04-27 04:01:00 -1100

    Job.java

--- Job.java
+++ Job.java
@@ -1,30 +1,31 @@
 import java.io.Serializable;
-public class Job implements Serializable{
+
+public class Job implements Serializable {
     private String jobid;
-    private String companyname;
     private String title;
     private String description;
     private double salary;
-    public Job(String jobid,String companyname,String title,String description,double salary){
-        this.jobid=jobid;
-        this.companyname=companyname;
-        this.title=title;
-        this.description=description;
-        this.salary=salary;
+
+    public Job(String jobid, String title, String description, double salary) {
+        this.jobid = jobid;
+        this.title = title;
+        this.description = description;
+        this.salary = salary;
     }
-    public String getJobid(){
+
+    public String getJobId() {
         return jobid;
     }
-    public String getCompanyname(){
-        return companyname;
-    }
-    public String getTitle(){
+
+    public String getTitle() {
         return title;
     }
-    public String getDescription(){
+
+    public String getDescription() {
         return description;
     }
-    public double getSalary(){
+
+    public double getSalary() {
         return salary;
     }
 }
