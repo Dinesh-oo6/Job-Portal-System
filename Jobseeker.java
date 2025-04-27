6a098b000878f98c18fb97b1f7b21678d54e4d1e
Dinesh-oo6 <dinexhoo6oo@gmail.com>
2025-04-27 03:59:44 -1100

    Jobseeker.java

--- Jobseeker.java
+++ Jobseeker.java
@@ -1,10 +1,12 @@
-public class Jobseeker extends User{
+class Jobseeker extends User {
     private String resume;
-    public Jobseeker(String id,String name,String email,String password,String resume){
-        super(id,name,email,password);
-        this.resume=resume;
+    
+    public Jobseeker(String email, String password, String resume) {
+        super(email, password);
+        this.resume = resume;
     }
-    public String getResume(){
+
+    public String getResume() {
         return resume;
     }
 }
