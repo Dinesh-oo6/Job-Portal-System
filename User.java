9b0c500e7813187bc0108d19fa27924c01592e13
Dinesh-oo6 <dinexhoo6oo@gmail.com>
2025-04-27 03:54:05 -1100

    User.java

--- User.java
+++ User.java
@@ -1,25 +1,18 @@
 import java.io.Serializable;
-public class User implements Serializable{
-    protected String id;
-    protected String name;
+
+public class User implements Serializable {
     protected String email;
     protected String password;
-    public User(String id,String name,String email,String password){
-        this.id=id;
-        this.name=name;
-        this.email=email;
-        this.password=password;
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
     }
-    public String getId(){
-        return id;
-    }
-    public String getName(){
-        return name;
-    }
-    public String getEmail(){
+
+    public String getEmail() {
         return email;
     }
-    public boolean validatepassword(String Password){
-        return this.password.equals(Password);
+
+    public boolean validatePassword(String password) {
+        return this.password.equals(password);
     }
 }
